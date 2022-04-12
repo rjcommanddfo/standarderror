841bf9881b083e76f9e9cf0231f2b8929caa61e1
rjcommanddfo <rylan.command@dfo-mpo.gc.ca>
2022-04-12 16:27:10 -0230

    added function

--- R/std_err.R
+++ R/std_err.R
@@ -1,15 +1,18 @@
 #' @title Calculate standard error
-#' @description Using a vector of samples, calculate the standard error of the mean.
+#' @description Using a vector of samples, calculate the standard error of the
+#'     mean.
 #'
-#' @param x
+#' @param x A vector of numerical data.
 #'
-#' @return
+#' @return A numeric value.
 #' @export
 #'
 #' @examples
+#'     std_err(x = c(4, 7, 2, 7, 5, 9))
+#'
 std_err <- function(x) {
 
-  x.std_err <- sd(x) / sqrt(length(x))
+  x.std.err <- stats::sd(x) / sqrt(length(x))
 
   return(x.std.err)
 
